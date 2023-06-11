f682f14107494a52fbce22086d872b92296d44b1
meilizheng <116130097+meilizheng@users.noreply.github.com>
2023-06-11 12:05:24 -0700

    fix and update
    fix and update code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace ClassLibrary
+﻿using MyClassLibrary;
+
+namespace ClassLibrary
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Class Libraries
+            //connect the  class library to application
+            List<Vehicle> _vehicles = new List<Vehicle>();
+            _vehicles.Add(new Land("Aston Martin"));
+            _vehicles.Add(new Sea("SSLetzSea"));
+            _vehicles.Add(new Air("Moiz"));
+            _vehicles.Add(new Plane("Boeing 747"));
+            _vehicles.Add(new Helicopter("Bell AH-1 Cobra"));
+
+            foreach (Vehicle vehicle1s in _vehicles)
+            {
+                Console.WriteLine(vehicle1s);
+            }
         }
     }
 }
